2402621407e3f4f5f48cfc7f1e2ba36d80684e1d
Jeremiah <jerry2121121021@yahoo.com>
2019-03-15 00:02:44 -0700

    Added the Continue button UI, Fixed some sound bugs and anchors

--- KingdomSurvivalProject/Assets/JT/MainMenuButtons.cs
+++ KingdomSurvivalProject/Assets/JT/MainMenuButtons.cs
@@ -8,6 +8,11 @@ public class MainMenuButtons : MonoBehaviour
 {
     public GameObject PatchNotesPanel;
     public GameObject ExitConfirmCanvas;
+    public GameObject MainButtonsGameObject;
+    [Header("Continue Screen GameObjects")]
+    public GameObject SelectedSavedGameInfoTab;
+    public GameObject ContinueCanvas;
+    public GameObject NotSelectingSavedGameWhenLoading;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +22,21 @@ public class MainMenuButtons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && MainButtonsGameObject.activeSelf == true)
         {
             ExitConfirmCanvas.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && MainButtonsGameObject.activeSelf == false && ContinueCanvas.activeSelf == true)
+        {
+            MainButtonsGameObject.SetActive(!MainButtonsGameObject.activeSelf);
+            ContinueCanvas.SetActive(!ContinueCanvas.activeSelf);
+            SelectedSavedGameInfoTab.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1) && ContinueCanvas.activeSelf == true && SelectedSavedGameInfoTab.activeSelf == true || Input.GetKeyDown(KeyCode.Mouse1) && ContinueCanvas.activeSelf == true && NotSelectingSavedGameWhenLoading.activeSelf == true)
+        {
+            GetComponent<AudioSource>().Play();
+            SelectedSavedGameInfoTab.SetActive(false);
+            NotSelectingSavedGameWhenLoading.SetActive(false);
         }
     }
     public void NewGame()
@@ -29,7 +46,11 @@ public class MainMenuButtons : MonoBehaviour
     }
     public void Continue()
     {
-
+        GetComponent<AudioSource>().Play();
+        MainButtonsGameObject.SetActive(!MainButtonsGameObject.activeSelf);
+        ContinueCanvas.SetActive(!ContinueCanvas.activeSelf);
+        PatchNotesPanel.SetActive(false);
+        SelectedSavedGameInfoTab.SetActive(false);
     }
     public void Settings()
     {
@@ -58,5 +79,28 @@ public class MainMenuButtons : MonoBehaviour
     {
         ExitConfirmCanvas.SetActive(!ExitConfirmCanvas.activeSelf);
         GetComponent<AudioSource>().Play();
+    }
+    public void LoadSavedGame()
+    {
+        if (SelectedSavedGameInfoTab.activeSelf == true)
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            NotSelectingSavedGameWhenLoading.SetActive(true);
+        }
+    }
+    public void SelectSave()
+    {
+        GetComponent<AudioSource>().Play();
+        NotSelectingSavedGameWhenLoading.SetActive(false);
+        SelectedSavedGameInfoTab.SetActive(true);
+    }
+    public void DeleteSave()
+    {
+        GetComponent<AudioSource>().Play();
+        SelectedSavedGameInfoTab.SetActive(false);
     }
 }
